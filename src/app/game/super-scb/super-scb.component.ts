2018c3d6b92ef3ebb40dca02ff4e31229fe69e42
WinUP <l.gholoi@outlook.com>
2020-08-01 09:41:46 +0800

    Add rpg download link

--- src/app/game/super-scb/super-scb.component.ts
+++ src/app/game/super-scb/super-scb.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
     styleUrls: ['./super-scb.component.scss']
 })
 export class SuperScbComponent {
-    public download(): void {
-        window.open('https://www.freem.ne.jp/dl/win/20067', '_blank');
+    public download(destination: string): void {
+        switch (destination) {
+            case 'freem':
+                window.open('https://www.freem.ne.jp/dl/win/20067', '_blank');
+                break;
+            case 'baidu':
+                window.open('https://pan.baidu.com/s/1CVZg1gxNv8-tE5SDg4bx8A', '_blank');
+                break;
+            case 'mega':
+                window.open('https://mega.nz/file/MB4mgCpR', '_blank');
+                break;
+        }
     }
 }
