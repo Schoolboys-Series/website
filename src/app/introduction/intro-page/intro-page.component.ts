acdb7fd46f977184cea48201dc432a9508f0fcc3
WinUP <l.gholoi@outlook.com>
2020-08-02 17:28:31 +0800

    Update link

--- src/app/introduction/intro-page/intro-page.component.ts
+++ src/app/introduction/intro-page/intro-page.component.ts
@@ -12,10 +12,10 @@ export class IntroPageComponent {
                 window.open('https://www.freem.ne.jp/dl/win/20067', '_blank');
                 break;
             case 'baidu':
-                window.open('https://pan.baidu.com/s/1CVZg1gxNv8-tE5SDg4bx8A', '_blank');
+                window.open('https://pan.baidu.com/s/1FbYcLMa94KE8sSbjtB_ROg', '_blank');
                 break;
             case 'mega':
-                window.open('https://mega.nz/file/MB4mgCpR#o27MmejDONjutoKmKUU-DvTljQOGdTKxQoSCw3_8i2U', '_blank');
+                window.open('https://mega.nz/folder/8YADFIgT#XQOp9ph3PZCtZg8B7az0sw', '_blank');
                 break;
         }
     }
