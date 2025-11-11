9ba3e7a29fe759e59e9828bad958bf2debc72935
AdorN <40393619+Ador-N@users.noreply.github.com>
2025-11-11 14:55:16 +0800

    Reuse GameModule in intro page to eliminate duplication

--- src/app/introduction/introduction.module.ts
+++ src/app/introduction/introduction.module.ts
@@ -6,14 +6,12 @@ import { NgModule } from '@angular/core';
 
 import { IntroPageComponent } from './intro-page/intro-page.component';
 import { ExtraComponentsModule } from '../extra-components/extra-components.module';
+import { GameModule } from '../game/game.module';
 
-@NgModule({ declarations: [
-        IntroPageComponent
-    ],
-    exports: [
-        IntroPageComponent
-    ], imports: [CommonModule,
-        MatListModule,
-        MatButtonModule,
-        ExtraComponentsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class IntroductionModule { }
+@NgModule({
+    declarations: [IntroPageComponent],
+    exports: [IntroPageComponent],
+    imports: [CommonModule, MatListModule, MatButtonModule, ExtraComponentsModule, GameModule],
+    providers: [provideHttpClient(withInterceptorsFromDi())]
+})
+export class IntroductionModule {}
